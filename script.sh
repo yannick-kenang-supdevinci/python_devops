0cba127715b9a11d63e40f9141815298cfdb5f7d
AliMokh <ali.mokh.2013@ieee.org>
2024-10-17 10:53:37 +0200

    add all files

---
+++ script.sh
@@ -0,0 +1,10 @@
+mkdir Project
+touch Project/requirements.txt
+echo "requests==2.25.1" > Project/requirements.txt
+echo "flask==1.1.2" >> Project/requirements.txt
+touch Project/makefile
+touch Project/.env
+touch Project/.gitignore
+echo "venv/" > Project/.gitignore
+touch Project/README.md
+touch Project/app.py
