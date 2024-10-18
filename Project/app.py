e5425053ca930768e31c02cf91b32c27ef521bb3
AliMokh <ali.mokh.2013@ieee.org>
2024-10-18 09:27:45 +0200

    add lecture 2 components

---
+++ Project/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from utils import add_numbers, subtract_numbers
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.json
+    result = add_numbers(data['a'], data['b'])
+    return jsonify({"result": result})
+
+@app.route('/subtract', methods=['POST'])
+def subtract():
+    data = request.json
+    result = subtract_numbers(data['a'], data['b'])
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
